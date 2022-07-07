bcad4b13cf21a3f2089c356a708511548c7a745d
Osver Fernandez <osverdev@icloud.com>
2022-07-07 06:42:14 -0600

    shape the rectangles

--- rectanglesIOS/ContentView.swift
+++ rectanglesIOS/ContentView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        
+        GeometryReader{ geo in
+            Rectangle()
+                .foregroundColor(Color.green)
+            Rectangle()
+                .foregroundColor(Color.red)
+                .frame(width: geo.size.width/3*2, height: geo.size.height/8, alignment: .center)
+                .padding(.top,geo.size.height/16*3)
+                .padding(.leading,geo.size.width/6)
+            Rectangle()
+                .foregroundColor(Color.purple)
+                .frame(width: geo.size.width/2, height: geo.size.height/2, alignment: .center)
+                .padding(.top,geo.size.height/2)
+            Rectangle()
+                .foregroundColor(Color.orange)
+                .frame(width: geo.size.width/2, height: geo.size.height/2, alignment: .leading)
+                .padding(.top,geo.size.height/2)
+                .padding(.leading,geo.size.width/2)
+        }.ignoresSafeArea()
+        
     }
 }
 
