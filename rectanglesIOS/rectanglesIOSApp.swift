d107510f151f2907b85ad3bb9347744f501c7507
Osver Fernandez <osverdev@icloud.com>
2022-07-07 05:42:05 -0600

    Initial Commit

---
+++ rectanglesIOS/rectanglesIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  rectanglesIOSApp.swift
+//  rectanglesIOS
+//
+//  Created by Osver Fernandez on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct rectanglesIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
